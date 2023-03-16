1020760fa94ce52e876cf42aca77dd73bb717d9d
FabianLeviDLC <AL02979388@TECMILENIO.MX>
2023-03-16 14:06:49 -0600

    Iniciamos Cambios de Actividad 8

---
+++ resources/views/empleado/create.blade.php
@@ -0,0 +1,24 @@
+Formulario de creación 
+
+<form action = "" method = "post"  enctype ="multipart/form-data">
+
+<label for = "Nombre"> Ingresa el Nombre </label>
+<input type = "Text" name = "Nombre" id = "Nombre">
+<br>
+
+<label for = "Super_Nombre"> Ingresa el Super Nombre </label>
+<input type = "Text" name = "Super_Nombre"  id = "Super_Nombre" >
+<br>
+
+<label for = "Foto"> Inserta la Foto </label>
+<input type = "file" name = "Foto" id = "Foto">
+<br>
+
+<label for = "Info_Extra"> Ingresa algo Extra </label>
+<input type = "Text" name = "Info_Extra" id = "Info_Extra">
+<br>
+
+<input type = "Submit" name = "Enviar" id = "Enviar">
+<br>
+
+</form>
